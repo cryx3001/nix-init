4cdc929dc90d762eb106b487c19f249fea9ca170
Titouan Guionneau <titouan.guionneau@epita.fr>
2022-08-23 11:49:28 +0200

    Maybe fixed mistake

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 echo "Creating / updating needed channel and preparing nvim config"
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable 
-nix-channel --update & git clone https://github.com/cryx3001/nvim-config.git ~/.config/nvim
+nix-channel --update 
 
 nix-env -iA nixos-unstable.home-manager
+
+git clone https://github.com/cryx3001/nvim-config.git ~/.config/nvim &
 
 mkdir -p ~/.config/nixpkgs
 cp ./home.nix ~/.config/nixpkgs/
