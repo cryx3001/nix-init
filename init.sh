7ff855f2b9de23140e917ed1ff563b4d4e4781c3
Titouan Guionneau <titouan.guionneau@epita.fr>
2022-11-14 15:02:49 +0100

    feat: Warning message if the nvim-config repo already exists

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
+#!/bin/sh
+
 echo "Creating / updating needed channel and preparing nvim config"
 nix-channel --add https://nixos.org/channels/nixos-22.05 nixos-stable
 nix-channel --update
 
 nix-env -iA nixos-stable.home-manager
 
-git clone https://github.com/cryx3001/nvim-config.git ~/.config/nvim &
+git clone https://github.com/cryx3001/nvim-config.git ~/.config/nvim
+clone_fail="$?"
 
 rm -rf ~/.config/nixpkgs
 mkdir -p ~/.config/nixpkgs
@@ -12,3 +15,8 @@ cp -r ./nixpkgs/* ~/.config/nixpkgs/
 
 echo "Installing home packages"
 home-manager switch
+
+if [ "$clone_fail" -ne "0" ]; then
+    printf "\n\e[1;33mThe clone of the neovim config failed!\nIf the respository already exists, you should try to pull for the latest updates\n"
+fi
+
